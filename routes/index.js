0373229698030e40a0abb11cf6337734be83f496
jdaniel <jdaniel@local>
2015-06-26 13:16:33 +0200

    Autoload

--- routes/index.js
+++ routes/index.js
@@ -3,16 +3,20 @@ var router = express.Router();
 
 var quizController = require('../controllers/quiz_controller.js');
 
-/* GET home page. */
+// Página de entrada
 router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
 });
 
+// Autoload de comandos con :quizId
+router.param('quizId', quizController.load); // autoload :quizId
+
 // Definición de rutas de /quizes
-router.get('/quizes',                      quizController.index);
-router.get('/quizes/:quizId(\\d+)',        quizController.show);
+router.get('/quizes', quizController.index);
+router.get('/quizes/:quizId(\\d+)', quizController.show);
 router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
 
+// Página de créditos
 router.get('/author', quizController.author);
 
 module.exports = router;
